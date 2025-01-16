a4f900ae31ae00d1101e61fbe5dc36c06324c222
Mir Khaled <mirs.sadat35@gmail.com>
2025-01-16 11:40:54 -0500

    file deleted, more codes added

--- src/test/java/testComponents/BaseTest.java
+++ src/test/java/testComponents/BaseTest.java
@@ -1,4 +1,16 @@
 package testComponents;
 
+import org.openqa.selenium.WebDriver;
+
+import java.util.Properties;
+
 public class BaseTest {
+
+    public static WebDriver driver;
+    public WebDriver initializeDriver(){
+
+        Properties prop = new Properties();
+
+        return driver;
+    }
 }

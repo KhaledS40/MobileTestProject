8f710dc50eb4dcb35bfbcdc0123ccd0e710d80e7
Mir Khaled <mirs.sadat35@gmail.com>
2025-01-16 13:20:48 -0500

    new changes

--- src/test/java/appiumBasics/Example.java
+++ src/test/java/appiumBasics/Example.java
@@ -5,6 +5,12 @@ public class Example {
 	public static void main(String[] args) {
 	
 		System.out.println("this is an example");
+		System.out.println("this is an example");
+		System.out.println("this is an example");
+		System.out.println("this is an example");
+		System.out.println("this is an example");
+		
+		System.out.println("this is an example");
 
 	}
 
